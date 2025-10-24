dad0ea1f04fb2a1ad0d34ac32a8334d85912d17c
ebobo020 <ena032397@gmail.com>
2025-10-24 17:12:17 +0900

    deploy

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        mint: {
+          50: '#f5fefb',
+          100: '#d9fbe5',
+        }
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
